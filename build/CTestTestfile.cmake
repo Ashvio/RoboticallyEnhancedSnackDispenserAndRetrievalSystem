34c45adf07992eadcb3f47cc8757ce633ed97a9b
Ashvin Govil <ashvin@cs.utexas.edu>
2016-05-02 18:36:57 -0500

    Workspace initialized

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashvin/final_workspace/src
+# Build directory: /home/ashvin/final_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
