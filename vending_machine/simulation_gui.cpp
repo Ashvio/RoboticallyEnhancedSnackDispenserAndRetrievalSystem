463c1f261c65bae286dbb075f93c27adbb9d2a76
jkcooley <cooley.jk8@gmail.com>
2016-05-11 15:22:39 -0500

    Worked on simulation

--- vending_machine/simulation_gui.cpp
+++ vending_machine/simulation_gui.cpp
@@ -34,13 +34,15 @@
 
 typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
 
-using namespace std;
+//using namespace std;
 
 //found using rviz 
-final double[] VENDING_MACHINE_POSE = {16.98, 3.75, 0.0, 0.0, 0.0 , -0.096, 0.99};
+double VENDING_MACHINE_POSE[] = {16.98, 3.75, 0.0, 0.0, 0.0, -0.096, 0.99};
+Client client("/action_executor/execute_plan", true);
 
 //uses move_base_client code to go to the preset vending machine location
-void goToMachine(bwi_msgs::QuestionDialog question)
+void goToMachine(bwi_msgs::QuestionDialog question, ros::ServiceClient client_gui, 
+				 int& argc, char** argv)
 {
   ros::init(argc, argv, "move_base_client");
   ros::NodeHandle n;
@@ -73,15 +75,15 @@ void goToMachine(bwi_msgs::QuestionDialog question)
 
   bool placed = false;
 
-  while(placed)
+  while(!placed)
   {
   	question.request.message = "Please place the snack on my shelf! Has the item been placed? ";
 	
-        for (unsigned int k = 0; k < doors.size(); k++){
-                question.request.options.push_back(doors.at(k));
-        }
-        question.request.timeout = 30.0;
+        question.request.options.push_back("Yes");
+        question.request.options.push_back("No");
 
+        question.request.timeout = 30.0;
+		int current_door = 0;
         if (client_gui.call(question))
         {
                 if (question.response.index >= 0){
@@ -95,10 +97,42 @@ void goToMachine(bwi_msgs::QuestionDialog question)
         else
         {
                 ROS_ERROR("Failed to call service /question_dialog");
-                return 1;
+                return;
         }
+        placed  = question.response.text == "Yes";
   }
 
+}
+
+void goToDoor(std::string location) {
+	
+	ROS_INFO_STREAM("going to " << location);
+
+	bwi_kr_execution::ExecutePlanGoal goal;
+
+	bwi_kr_execution::AspRule rule;
+	bwi_kr_execution::AspFluent fluent;
+	fluent.name = "not facing";
+
+	fluent.variables.push_back(location);
+
+	rule.body.push_back(fluent);
+	goal.aspGoal.push_back(rule);
+
+	ROS_INFO("sending goal");
+	client.sendGoalAndWait(goal);
+
+	if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+	  ROS_INFO("Aborted");
+	} else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+	  ROS_INFO("Preempted");
+	}
+
+	else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+	  ROS_INFO("Succeeded!");
+	} else {
+	  ROS_INFO("Terminated");
+	}
 }
 
 int main(int argc, char**argv) {
@@ -107,7 +141,7 @@ int main(int argc, char**argv) {
 
   ros::NodeHandle privateNode("~");
   
-  std::vector<string> doors;
+  std::vector<std::string> doors;
 
   doors.push_back("d3_414b1");
   doors.push_back("d3_414b2");
@@ -116,13 +150,12 @@ int main(int argc, char**argv) {
   doors.push_back("d3_418");
   int current_door = 0, snack_dest = 0;
 
-  std::vector<string> snacks;
+  std::vector<std::string> snacks;
   
   snacks.push_back("goldfish");
   snacks.push_back("izze");
   snacks.push_back("none");
 
-  Client client("/action_executor/execute_plan", true);
   client.waitForServer();
 
   ros::ServiceClient client_gui = n.serviceClient<bwi_msgs::QuestionDialog>("/question_dialog");
@@ -132,67 +165,46 @@ int main(int argc, char**argv) {
 	//use gui to decide which door to go to
 	bwi_msgs::QuestionDialog question;
 	question.request.type = question.request.CHOICE_QUESTION;
-
-	//TODO: ask for snack instead, go to the next door in the end	
-	question.request.message = "Please select a snack: ";
-	for (unsigned int k = 0; k < doors.size(); k++){
-		question.request.options.push_back(doors.at(k));
-	}
-	question.request.timeout = 30.0;
-	
-	if (client_gui.call(question))
-    	{
-		if (question.response.index >= 0){
-			current_door = question.response.index;
+	for (int i = 0; i < doors.size(); i++) {
+		std::string location = doors.at(i);
+		goToDoor(location);
+		//TODO: ask for snack instead, go to the next door in the end	
+		question.request.message = "Please select a snack: ";
+		for (unsigned int k = 0; k < snacks.size(); k++){
+			question.request.options.push_back(snacks.at(k));
 		}
-		else {
-			ROS_INFO("No response detected, defaulting to 0");
-			current_door = 0;
+		question.request.timeout = 30.0;
+		
+		if (client_gui.call(question))
+			{
+			if (question.response.index >= 0){
+				current_door = question.response.index;
+			}
+			else {
+				ROS_INFO("No response detected, defaulting to 0");
+				current_door = 0;
+			}
 		}
-	}
-	else
-	{
-		ROS_ERROR("Failed to call service /question_dialog");
-		return 1;
-	}
+		else
+		{
+			ROS_ERROR("Failed to call service /question_dialog");
+			return 1;
+		}
 
-	string location = doors.at(current_door);
 
-//if a snack was requested, location = vending machine
-	if(!request.equals("none"))
-	{
-    		location = VENDING_MACHINE_POSE;
-	}
-
-	//do this
-    ROS_INFO_STREAM("going to " << location);
-
-    bwi_kr_execution::ExecutePlanGoal goal;
-
-    bwi_kr_execution::AspRule rule;
-    bwi_kr_execution::AspFluent fluent;
-    fluent.name = "not facing";
-
-    fluent.variables.push_back(location);
-
-    rule.body.push_back(fluent);
-    goal.aspGoal.push_back(rule);
-
-    ROS_INFO("sending goal");
-    client.sendGoalAndWait(goal);
-
-    if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
-      ROS_INFO("Aborted");
-    } else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
-      ROS_INFO("Preempted");
-    }
-
-    else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-      ROS_INFO("Succeeded!");
-    } else
-      ROS_INFO("Terminated");
+	//if a snack was requested, location = vending machine
+		if(question.response.index != question.request.NO_RESPONSE || question.response.index != question.request.TIMED_OUT)
+		{
+			goToMachine(question,  client_gui, argc, argv);
+			goToDoor(location);
+		}
+		
+		//do this
+		
+		
 
   }
 
   return 0;
+}
 }
